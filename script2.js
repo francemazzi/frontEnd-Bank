2df79235e373c62e199bbf1313b5d712eb38de8f
FraSma <francemazzi@gmail.com>
2022-08-14 14:59:17 +0200

    ottimizzazione observer

--- script2.js
+++ script2.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const cards = document.querySelectorAll('.card');
+//Per rendere il codice performanete usiakmo observer e criamo una nuova interessazione
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    entry.target.classList.toggle('show', entry.isIntersecting);
+  });
+});
+
+//Ora osserviamo le interesazioni di un elemnto nel nostro schermo.
+
+cards.forEach(card => {
+  observer.observe(card);
+});
