cc1899ec0319c110e9463303da77ec4de0e5efa7
FraSma <francemazzi@gmail.com>
2022-08-15 16:36:15 +0200

    correzione card

--- script2.js
+++ script2.js
@@ -1,15 +1,50 @@
 'use strict';
 
 const cards = document.querySelectorAll('.card');
+const cardContainer = document.querySelector('.card-container');
 //Per rendere il codice performanete usiakmo observer e criamo una nuova interessazione
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    entry.target.classList.toggle('show', entry.isIntersecting);
-  });
-});
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      entry.target.classList.toggle('show', entry.isIntersecting);
+      if (entry.isIntersecting) {
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 1,
+  }
+);
+
+const obsLastCard = new IntersectionObserver(
+  entries => {
+    const lastcard = entries[0];
+    if (!lastcard.isIntersecting) return;
+    loadNewCard();
+    obsLastCard.unobserve(lastcard.target);
+    obsLastCard.observe(document.querySelector('.card:last-child'));
+  },
+  {
+    //ottimo per inserire margini e caricare in anticipo immagini o contenuti
+    rootMargin: '100px',
+  }
+);
+
+obsLastCard.observe(document.querySelector('.card:last-child'));
 
 //Ora osserviamo le interesazioni di un elemnto nel nostro schermo.
 
 cards.forEach(card => {
   observer.observe(card);
 });
+
+function loadNewCard() {
+  for (let i = 0; i < 10; i++) {
+    const card = document.createElement('div');
+    card.textContent = 'New Card';
+    card.classList.add('card');
+    observer.observe(card);
+    cardContainer.append(card);
+  }
+}
